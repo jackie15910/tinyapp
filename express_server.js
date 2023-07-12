20aebcdfffc363ac0f2a0655717e2dd07eb25a3e
Jackie <jackie15910@gmail.com>
2023-07-12 18:52:04 -0400

    Changed usernames to id

--- express_server.js
+++ express_server.js
@@ -44,7 +44,7 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {username: req.cookies.username};
+  const templateVars = {user_id: req.cookies.user_id};
   res.render("urls_new", templateVars);
 });
 
@@ -53,12 +53,12 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies.username};
+  const templateVars = { urls: urlDatabase, user_id: req.cookies.user_id};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user_id: req.cookies.user_id };
   res.render("urls_show", templateVars);
 });
 
