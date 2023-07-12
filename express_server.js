9b6dfc26dc876c4e78383df99fbf5dfc8ad53d3e
Jackie <jackie15910@gmail.com>
2023-07-12 15:52:34 -0400

    Added login functionality

--- express_server.js
+++ express_server.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
 
 function generateRandomString(length = 6) {
   const alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let randomString = '';
-  
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * alphanumeric.length);
     randomString += alphanumeric.charAt(randomIndex);
   }
-  
   return randomString;
 }
 
@@ -26,7 +27,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies.username};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -34,12 +36,13 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.username};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(req.body.username);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
 
@@ -59,7 +62,7 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login", (req, res) => {
   console.log(req.body.username);
-  res.cookie('userName', req.body.username);
+  res.cookie('username', req.body.username);
   res.redirect("/urls");
 });
 
