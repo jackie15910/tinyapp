ccecb74aace11bd69a38e7de8009a0700cbe7fd6
Jackie <jackie15910@gmail.com>
2023-07-15 14:37:33 -0400

    Updated database comments

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 const users = {
-  // id: "temporary", 
-  // email: "temporary", 
-  // password: "temporary"
+  // id: "userID", 
+  // email: "email", 
+  // password: "hashed password"
 };
 
 const urlDatabase = {
+  //  id = {longURL: "longURL",
+  //        userID: "userID"};
 };
 
 module.exports = {users, urlDatabase};
